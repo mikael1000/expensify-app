e901208405cc7f7233b07683bc95ef5954c36bd0
mikael1000 <mikael.johansson.1000@live.se>
2019-03-31 12:01:51 +0200

    Setup startRemoveExpense

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
-import { addExpense } from './actions/expenses'
+import { startSetExpenses } from './actions/expenses'
 import { setTextFilter } from './actions/filters'
 import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
@@ -19,9 +19,13 @@ const jsx = (
   </Provider>
 )
 
-ReactDOM.render(jsx, document.getElementById('app')) // IndecisionApp
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'))
+
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx, document.getElementById('app'))
+})
 
 /*
 --------------------------------------
-ypok: 149
+ypok: 
 */
--- src/tests/expenses-test.js
+++ src/tests/expenses-test.js
@@ -1,2 +1,14 @@
 import selectExpensesTotal from '../selectors/expenses-total'
 import expenses from './fixtures/expenses'
+import { database } from 'firebase'
+
+beforeEach(() => {
+  const expensesData = {}
+  expenses.forEach(({ id, description, note, amount, createdAt }) => {
+    expensesData[id] = { description, note, amount, createdAt }
+  })
+  database
+    .ref('expenses')
+    .set(expensesData)
+    .then(() => done())
+})
